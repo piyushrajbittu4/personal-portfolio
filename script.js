6860df351467a7604c4ed66d786e1da30056ee5e
piyushrajbittu4 <116016972+piyushrajbittu4@users.noreply.github.com>
2024-06-18 16:31:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+  
+      const targetSection = document.querySelector(this.getAttribute('href'));
+      targetSection.scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+});
